80492139369564d381a489520c9e49a449554286
SvenJuergens <sj@nordsonne.de>
2019-01-14 16:30:40 +0100

    [TASK] move content of ext_tables.php to new Files see: Breaking: #78384 - Frontend ignores TCA in ext_tables.php
    https://docs.typo3.org/typo3cms/extensions/core/Changelog/8.5/Breaking-78384-FrontendIgnoresTCAInExtTables.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,10 @@
 <?php
-if (!defined ('TYPO3_MODE')) die('Access denied.');
+defined('TYPO3_MODE') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/PageBrowse.php', '_pi1', 'list_type', 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
+    'pagebrowse',
+    'Classes/PageBrowse.php',
+    '_pi1',
+    'list_type',
+    1
+);
